851b3908ff65e1b931a712c0830192c1ec7d76cb
Jan Gosmann <jan@hyper-world.de>
2013-07-20 17:49:48 +0200

    Revert "Allow multiple paths"
    This reverts commit 67077af37e75cbcd56ee5e870ef26761f4d8becf.
    
    Conflicts:
    	shardpile.py

--- shardpile.py
+++ shardpile.py
@@ -172,7 +172,7 @@ if __name__ == '__main__':
         "tree in the file system and allows to verify trees against this " +
         "hash database.")
     parser.add_argument(
-        'paths', nargs='*', type=str, help="Paths to update hashes for.")
+        'path', nargs=1, type=str, help="Path to update hashes for.")
     parser.add_argument(
         '-d', '--database', nargs=1, type=str, default=['~/.shardpile.db'],
         help="Hash database to update.")
@@ -186,30 +186,24 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     with HashDb(os.path.expanduser(args.database[0])) as db:
-        if args.update and not args.no_strip:
-            db.strip()
-
-        for path in args.paths:
-            if args.update:
-                if os.path.isfile(path):
-                    db.update_path('.', path)
-                else:
-                    db.update_tree(path)
+        path = args.path[0]
+        if args.update:
+            if not args.no_strip:
+                db.strip()
+            if os.path.isfile(path):
+                db.update_path(path)
             else:
-                print(path)
-                print(len(path) * '-')
-                print('')
-                changed, missing_in_db, missing_on_disk = db.verify_tree(path)
-                print('Changed:')
-                for name in changed:
-                    print(name)
-                print('')
-                print('Missing in DB:')
-                for name in missing_in_db:
-                    print(name)
-                print('')
-                print('Missing on disk:')
-                for name in missing_on_disk:
-                    print(name)
-                print('')
-                print('')
+                db.update_tree(path)
+        else:
+            changed, missing_in_db, missing_on_disk = db.verify_tree(path)
+            print('Changed:')
+            for name in changed:
+                print(name)
+            print('')
+            print('Missing in DB:')
+            for name in missing_in_db:
+                print(name)
+            print('')
+            print('Missing on disk:')
+            for name in missing_on_disk:
+                print(name)
